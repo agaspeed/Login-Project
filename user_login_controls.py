7ec5402f87ae548a4a5e1e4622ec0c97abb6fbe4
Omar Gonzalez <ogonzalez@creativeelectron.com>
2022-06-23 14:02:58 -0700

    Shows Initial UI

--- user_login_controls.py
+++ user_login_controls.py
@@ -1,23 +1,26 @@
 
-from sys import get_coroutine_origin_tracking_depth
-from PySide2 import QtCore, QtWidgets, QtGui
+import sys
+
+from PySide2.QtWidgets import QApplication, QMainWindow
 
 # The reason to import this is because it was auto generated
 from forms.ce_login_main import Ui_Main_window
 
-class User_login_controls(QtWidgets.QWidget):
+class User_login_controls(QMainWindow):
 
-    def __init__(self, broker):
+    def __init__(self):
         super(User_login_controls, self).__init__()
-        print("Nice  Alma!")
-        self.broker = broker
+
         self.ui = Ui_Main_window()
         self.ui.setupUi(self)
 
-        #TODO: make the window show. 
+        # self.setWindowTitle("C.E.inc Hi Alma")
 
+        # Make Connections here
+        self.make_connections()
 
-        self.make_connections(self)
+        # Show UI 
+        self.show()
 
     def make_connections(self):
         # here is where you will make connections to the buttons and right click actions 
@@ -33,5 +36,13 @@ class User_login_controls(QtWidgets.QWidget):
 
     def show_controls(self):
         pass
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = User_login_controls()
+
+    sys.exit(app.exec_())
 
 
